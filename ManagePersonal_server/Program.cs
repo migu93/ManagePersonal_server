b8da193a019d5841ee84b106806e32896dabe478
user <njkita007@yandex.ru>
2023-02-21 11:09:21 +0300

    Добавьте файлы проекта.

---
+++ ManagePersonal_server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ManagePersonal_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
