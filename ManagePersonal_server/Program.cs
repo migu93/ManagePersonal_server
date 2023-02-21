bef447ef12bef4ff060e9f934fd1037a34cc92b4
user <njkita007@yandex.ru>
2023-02-21 13:38:42 +0300

    separated in TcpServer.cs class

--- ManagePersonal_server/Program.cs
+++ ManagePersonal_server/Program.cs
@@ -10,39 +10,15 @@ namespace ManagePersonal_server
     {
         static void Main(string[] args)
         {
-            
-            
             const string ip = "127.0.0.1";
             const int port = 5000;
 
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var server = new TcpServer();
+            server.StartListening(ip, port);
 
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            tcpSocket.Bind(tcpEndPoint);
-            tcpSocket.Listen(5);
-
-            while (true)
-            {
-                var listener = tcpSocket.Accept();
-                var buffer = new byte[256];
-                var size = 0;
-                var data = new StringBuilder();
-
-                do
-                {
-                    size = listener.Receive(buffer);
-                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
-
-                } while (listener.Available > 0);
-
-                Console.WriteLine(data);
-
-                listener.Send(Encoding.UTF8.GetBytes("Успех"));
-                
-                listener.Shutdown(SocketShutdown.Both);
-                listener.Close();
-            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
         }
     }
 }
---
+++ ManagePersonal_server/TcpServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ManagePersonal_server
+{
+    public class TcpServer
+    {
+        public void StartListening(string ip, int port)
+        {
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);
+
+            Console.WriteLine($"Server started at {ip}:{port}");
+
+            while (true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+                } while (listener.Available > 0);
+
+                Console.WriteLine("Received data: " + data);
+
+                listener.Send(Encoding.UTF8.GetBytes("Success"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
